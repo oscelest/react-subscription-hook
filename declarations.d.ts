bbb0029ab27266b02dfda01ef3f41c3f6829c372
oscelest <oscelest@gmail.com>
2023-03-04 23:28:39 +0100

    Updated code.

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,8 @@ declare module "*.module.css" {
   const content: {[className: string]: string};
   export = content;
 }
+
+type PrevStateFn<V> = (prev: V) => V
+type UpdateFn<V> = (value: V | PrevStateFn<V>) => void
+type NotFunction = string | number | boolean | null | undefined | bigint | readonly any[] | {apply?: never, [k: string]: any} | {call?: never, [k: string]: any};
+type SubscriptionUpdateCallbackFn<V extends NotFunction> = (value: V) => void
--- src/hooks/useSubscription.tsx
+++ src/hooks/useSubscription.tsx
@@ -1,39 +1,5 @@
 import {useEffect, useState} from "react";
-
-class Subscription<V extends NotFunction = NotFunction> {
-  
-  private value: V;
-  private readonly subscription_list: SubscriptionUpdateCallbackFn<V>[];
-  
-  constructor(value: V) {
-    this.value = value as V;
-    this.subscription_list = [];
-  }
-  
-  public getValue(): V {
-    return this.value;
-  }
-  
-  public setValue(value: V) {
-    this.value = value;
-    for (let fn of this.subscription_list) {
-      fn(this.value);
-    }
-    return value;
-  }
-  
-  public subscribe(callback: SubscriptionUpdateCallbackFn<V>) {
-    this.subscription_list.push(callback);
-  }
-  
-  public unsubscribe(callback: SubscriptionUpdateCallbackFn<V>) {
-    for (let i = this.subscription_list.length; i >= 0; i--) {
-      if (this.subscription_list[i] !== callback) continue;
-      delete this.subscription_list[i];
-    }
-  }
-}
-
+import {Subscription} from "../classes/Subscription";
 
 export function createSubscription<C extends NotFunction>(context: C): Subscription<C> {
   return new Subscription<C>(context);
@@ -65,7 +31,3 @@ export function useSubscription<V extends NotFunction>(subscription: Subscriptio
   }
 }
 
-type UpdateFn<V> = (value: V | PrevStateFn<V>) => void
-type PrevStateFn<V> = (prev: V) => V
-type NotFunction = string | number | boolean | null | undefined | bigint | readonly any[] | {apply?: never, [k: string]: any} | {call?: never, [k: string]: any};
-type SubscriptionUpdateCallbackFn<V extends NotFunction> = (value: V) => void
