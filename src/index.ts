a47d878a1d278091cc327a041facdac29c49f41c
oscelest <oscelest@gmail.com>
2023-03-04 22:13:08 +0100

    Updated readme

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import {createContext} from "react";
-import {Dialog} from "./classes";
-
-export const DialogContext = createContext<Dialog>({} as Dialog);
-export {useDialog, type UseDialogHook, type DialogIndexFn} from "./hooks";
+export * from "./hooks";
