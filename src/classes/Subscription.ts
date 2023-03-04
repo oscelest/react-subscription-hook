c5c1fe7df77eb7117f2aef8c6c0bc037feea9321
oscelest <oscelest@gmail.com>
2023-03-04 23:33:36 +0100

    Updated code.

--- src/classes/Subscription.ts
+++ src/classes/Subscription.ts
@@ -1,34 +1,33 @@
-
 export class Subscription<V extends NotFunction = NotFunction> {
   
-  private value: V;
-  private readonly subscription_list: SubscriptionUpdateCallbackFn<V>[];
+  #value: V;
+  readonly #subscription_list: SubscriptionUpdateCallbackFn<V>[];
   
   constructor(value: V) {
-    this.value = value as V;
-    this.subscription_list = [];
+    this.#value = value as V;
+    this.#subscription_list = [];
   }
   
   public getValue(): V {
-    return this.value;
+    return this.#value;
   }
   
   public setValue(value: V) {
-    this.value = value;
-    for (let fn of this.subscription_list) {
-      fn(this.value);
+    this.#value = value;
+    for (let fn of this.#subscription_list) {
+      fn(this.#value);
     }
     return value;
   }
   
   public subscribe(callback: SubscriptionUpdateCallbackFn<V>) {
-    this.subscription_list.push(callback);
+    this.#subscription_list.push(callback);
   }
   
   public unsubscribe(callback: SubscriptionUpdateCallbackFn<V>) {
-    for (let i = this.subscription_list.length; i >= 0; i--) {
-      if (this.subscription_list[i] !== callback) continue;
-      delete this.subscription_list[i];
+    for (let i = this.#subscription_list.length; i >= 0; i--) {
+      if (this.#subscription_list[i] !== callback) continue;
+      delete this.#subscription_list[i];
     }
   }
 }
