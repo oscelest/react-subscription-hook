9f2e20c9e7c0a442849fd8ea6b15ff0a25be235b
oscelest <oscelest@gmail.com>
2023-03-06 01:03:34 +0100

    Attempting a version that might be better for hot reloading.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {createSubscription, useSubscription} from "../src/hooks";
+import {createSubscription, useSubscription} from "../src";
 import Style from "./index.module.css";
 
 const subscription = createSubscription<number>(0);
 
 function IndexPage() {
   const [value, setValue] = useSubscription(subscription);
-  const [value2] = useSubscription(subscription);
+  const [value2, setValue2] = useSubscription(subscription);
   
   return (
     <div className={Style.Component}>
@@ -15,12 +15,17 @@ function IndexPage() {
         <span>{value}</span>
         <span>{value2}</span>
       </div>
-      <button onClick={onComponentClick}>Click me for dialog!</button>
+      <button onClick={onComponentClick}>Click me for 1!</button>
+      <button onClick={onComponentClick2}>Click me for 2!</button>
     </div>
   );
   
   function onComponentClick() {
     setValue(value + 1);
+  }
+  
+  function onComponentClick2() {
+    setValue2(value => value + 2);
   }
 }
 
